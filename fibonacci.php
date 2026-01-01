06131fb1817d98a72e89b83db8a2ffd06fa69e73
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-01-01 23:55:52 +0100

    happy new ywar

---
+++ fibonacci.php
@@ -0,0 +1,20 @@
+<?php 
+
+echo "<h1>Fibonacci</h1>";
+
+function fibonacci ($n){
+    
+
+    for($index = 0; $index<=$n; $index++){
+
+        while($index<=$n){
+            $index++;
+        }
+        $index = 0;
+    }
+
+}
+
+echo fibonacci(7);
+
+?>
