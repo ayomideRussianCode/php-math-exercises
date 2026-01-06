a44c23c9334ea1a012ac7b40491ff439d2933c3e
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-01-06 16:43:56 +0100

    started working on prime number checker

---
+++ prime-checker.php
@@ -0,0 +1,13 @@
+<?php
+
+function isPrime($num) {
+
+//take a number and divide it by 0,2,3,4,5,6,7,8,9 if it has a remainder
+
+$number = 2;
+
+
+}
+echo isPrime(17) ? "Prime" : "Not Prime";
+
+?>
