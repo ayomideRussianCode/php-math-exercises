259abf7b6debfc039eccfd832fd611d9624fbe3e
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-01-28 02:33:07 +0100

    gcd done

--- greatest-common-divisor.php
+++ greatest-common-divisor.php
@@ -2,8 +2,19 @@
 
 echo "<h1>Greatest Common Divisor</h1>";
 
-function gcd(){
+function findGcd($a, $b){
 
+    $minValue = min($a, $b);
+    $gcd = 0;
+
+    for($i =1; $i<=$minValue; $i++){
+
+        if($a % $i == 0 && $b % $i ==0){
+            $gcd = $i;
+        }
+    }
+    return $gcd;
 }
+echo "GCD: " . findGcd(20, 30);
 
 ?>
