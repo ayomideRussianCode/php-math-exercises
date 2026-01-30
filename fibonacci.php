ce8cc7f06f23c1187e898c54cfe42abd12722e23
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-01-30 23:42:46 +0100

    improving my fibonacci solution

--- fibonacci.php
+++ fibonacci.php
@@ -6,9 +6,9 @@ function fibonacci($n){
 
     $n1 = 1;
     $n2 = 0;
-    for($index = 0; $index <= $n; $index++){
+    for($index = 1; $index <= $n; $index++){
 
-      echo $n2. ',';
+       echo $n2. ',';
 
       $nth = $n1 + $n2;
       $n1 = $n2;
