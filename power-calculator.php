d2619539e403d6bab5eb0e374ea14bf7115ca003
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-01-31 10:53:39 +0100

    power calculator

--- power-calculator.php
+++ power-calculator.php
@@ -1,18 +1,33 @@
 <?php
 
-echo 'Hello';
+echo '<h1>Power Calculator</h1>';
 
 // $base = 2;
 // $exponent = 4;
 // $pow = 
 
-function calcPower($base, $exponent){
+// function calcPower($base, $exponent){
     
-    $pow =  $base ** $exponent;
+//     $pow =  $base ** $exponent;
 
-    return $pow;
+//     return $pow;
 
+// }
+// echo  calcPower(2, 4);
+
+
+function calcPower ($base , $exponent){
+
+    $power = 1;
+
+    for($i = 1; $i<= $exponent; $i++){
+
+        $power = $power * $base;
+    }
+
+    return $power;
 }
-echo  calcPower(2, 4);
+
+echo calcPower(2, 2);
 
 ?>
